a8f9b62e4487648f7eac16b628565248fc4e96a6
Imdoza3 <iamsteel2004@gmail.com>
2019-10-07 08:44:17 -0500

    Identifying strings

--- Week10/Week2.py
+++ Week10/Week2.py
@@ -1,6 +1,10 @@
-x = "There are %d types of people." % 10
+# this string is telling us the types of people
+x = "There are %d types of people." % 10\
+# this is a string
 binary = "binary"
+# and so is this one
 doNot = "don't"
+# this string tells us what they know
 y = "Those who know %s and those who %s." % (binary, doNot)
 
 print(x)
@@ -9,14 +13,15 @@ print(y)
 print("I said: %r" % x)
 print("I also said: '%s' ." % y)
 
+# hilarious + false because the joke was not funny
 hillarious = False
 joke_evaluation = "Isn't that joke so funny?! %r"
 print(joke_evaluation % hillarious)
 
+# these two are the two sides of a string
 w = "This is the left side of..."
 e= "a string with a right side."
 
 print(w + e)
-
 
 
