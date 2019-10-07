1d091528eeab1949028217eb872bcfc4b40a77f2
Imdoza3 <iamsteel2004@gmail.com>
2019-10-07 08:36:50 -0500

    pushing first part of todays project

---
+++ Week10/Week2.py
@@ -0,0 +1,22 @@
+x = "There are %d types of people." % 10
+binary = "binary"
+doNot = "don't"
+y = "Those who know %s and those who %s." % (binary, doNot)
+
+print(x)
+print(y)
+
+print("I said: %r" % x)
+print("I also said: '%s' ." % y)
+
+hillarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+print(joke_evaluation % hillarious)
+
+w = "This is the left side of..."
+e= "a string with a right side."
+
+print(w + e)
+
+
+
