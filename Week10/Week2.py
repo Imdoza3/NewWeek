88b46a15daa9c3016e8068fc29f0cf83344c225b
Imdoza3 <iamsteel2004@gmail.com>
2019-10-08 08:37:33 -0500

    second part of 10-8

--- Week10/Week2.py
+++ Week10/Week2.py
@@ -45,5 +45,10 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-print(end1)
+print(end1 + end2 + end3 + end4 + end5 + end6  + end7 + end8 + end9 + end10 + end11 + end12)
 
+# More formating
+formatter = "%r %r %r %r"
+print(formatter % (1, 2, 3, 4))
+print(formatter % ("one", "two", "three", "four"))
+print(formatter % ())
