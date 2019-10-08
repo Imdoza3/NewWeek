ba986f7f63f0d1bbacfe31d5c2b8da31f5fa42b4
Imdoza3 <iamsteel2004@gmail.com>
2019-10-08 08:31:48 -0500

    First part of 10-8

--- Week10/Week2.py
+++ Week10/Week2.py
@@ -25,3 +25,25 @@ e= "a string with a right side."
 print(w + e)
 
 
+# More stuff 10-8
+
+print("Mary had a little lamb.")
+print("Its fleece was what as %s." % 'snow')
+print("And everywhere that Mary went.")
+print("." * 10)
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print(end1)
+
