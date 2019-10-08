56b5467079ec7131f9e0f322dda9c156fda07e2c
Imdoza3 <iamsteel2004@gmail.com>
2019-10-08 08:46:45 -0500

    third part of 10-8

--- Week10/Week2.py
+++ Week10/Week2.py
@@ -45,10 +45,16 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
+
 print(end1 + end2 + end3 + end4 + end5 + end6  + end7 + end8 + end9 + end10 + end11 + end12)
 
 # More formating
 formatter = "%r %r %r %r"
 print(formatter % (1, 2, 3, 4))
 print(formatter % ("one", "two", "three", "four"))
-print(formatter % ())
+print(formatter % (True, False, False, True))
+print(formatter % (formatter, formatter, formatter, formatter))
+
+# Why did I use %r instead of %s
+
+
