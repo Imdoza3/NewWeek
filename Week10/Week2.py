641d08c15884f0ad73cd650c8717df4bf86d5b88
Imdoza3 <iamsteel2004@gmail.com>
2019-10-09 09:02:43 -0500

    Next part

--- Week10/Week2.py
+++ Week10/Week2.py
@@ -61,13 +61,15 @@ print(formatter % (formatter, formatter, formatter, formatter))
 days = "Mon Tue Wed Thu Fri Sat Sun"
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
 
-print()
-print()
+print("Here are the days: ", days)
+print("Here are the months: ", months)
 
-
-
-
-
+print("""
+There's something going on here.
+With the three double quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
 
 
 # What if I didnt like Jan being listed on the line with the rest of the
